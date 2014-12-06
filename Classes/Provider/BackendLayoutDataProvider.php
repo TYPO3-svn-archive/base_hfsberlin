abd22955b5f21272519383116a5b2575b8893157
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-06 21:50:53 +0000

    6.0.0 +++ #63630
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87131 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Provider/BackendLayoutDataProvider.php
+++ Classes/Provider/BackendLayoutDataProvider.php
@@ -59,17 +59,12 @@ class BackendLayoutDataProvider implements \TYPO3\CMS\Backend\View\BackendLayout
                 1 {
                   name = LLL:EXT:base_hfsberlin/Resources/Private/Language/Backend.xlf:backend_layout.column.content.main
                   colPos  = 0
-                  colspan = 8
+                  colspan = 9
                 }
                 2 {
                   name = LLL:EXT:base_hfsberlin/Resources/Private/Language/Backend.xlf:backend_layout.column.content.navigation
                   colPos  = 1
-                  colspan = 2
-                }
-                3 {
-                  name = LLL:EXT:base_hfsberlin/Resources/Private/Language/Backend.xlf:backend_layout.column.content.margin
-                  colPos  = 2
-                  colspan = 2
+                  colspan = 3
                 }
               }
             }
