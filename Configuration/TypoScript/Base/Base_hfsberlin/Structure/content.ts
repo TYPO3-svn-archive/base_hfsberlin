ce28c3344ad89081e219bf47ae399c83b0e45057
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-06 21:27:54 +0000

    6.0.0 +++ #63630
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87128 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/Base/Base_hfsberlin/Structure/content.ts
+++ Configuration/TypoScript/Base/Base_hfsberlin/Structure/content.ts
@@ -1,27 +1,25 @@
 base_hfsberlin =
 base_hfsberlin {
-  // content, footer, header
+  // content, header
   structure =
   structure {
     // main (0), margin (2), navigation (1)
     content =
     content {
-      // CONTENT, stdWrap
       main = COA
       main {
+        // table, select, stdWrap
         10 < styles.content.get
         10 {
           select {
             where = colPos=0
           }
-          slide >
           stdWrap {
             required  = 1
             wrap      = <!--contentmain--><!--TYPO3SEARCH_begin--><section class="content contentmain"><div class="row"><div class="small-12 columns"> |</div></div></section><!--TYPO3SEARCH_end--><!--/contentmain-->
           }
         }
       }
-      // CONTENT, stdWrap
       margin = COA
       margin {
         // table, select, stdWrap
@@ -33,7 +31,6 @@ base_hfsberlin {
           slide = -1
         }
       }
-      // CONTENT, stdWrap
       navigation = COA
       navigation {
         // table, select, stdWrap
