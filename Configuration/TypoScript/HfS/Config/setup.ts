3aa67fedf0f46ed0a5cb02eb1c30ea556be3231e
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-06 23:55:29 +0000

    6.0.0 +++ #63630
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87149 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Configuration/TypoScript/HfS/Config/setup.ts
@@ -0,0 +1,25 @@
+config {
+  admPanel        = 1
+
+  baseURL         = {$base_hfsberlin.htmlhead.base_href}
+  extTarget       = _blank
+
+  headerComment (
+        TYPO3-Programmierung und Hosting: die-netzmacher.de
+)
+
+  language        = de
+  locale_all = de_DE
+  metaCharset     = UTF-8
+  doctype         = html5
+  xmlprologue     = none
+  xhtml_cleaning  = all
+  htmlTag_langKey = de
+
+  spamProtectEmailAddresses               = 6
+  spamProtectEmailAddresses_atSubst       = <span style="display:none;">spamfilter</span><span class="dummy">@</span>
+  spamProtectEmailAddresses_lastDotSubst  = <span style="display:none;">spamfilter</span><span class="dummy">.</span>
+
+  tx_realurl_enable = 1
+
+}
