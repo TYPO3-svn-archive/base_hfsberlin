c6f5d883fe1ca26150bfa54f56637f759eb6a01a
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-06 21:31:14 +0000

    6.0.0 +++ #63630
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87129 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/Base/Start/setup.ts
+++
@@ -1,2 +0,0 @@
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/Base/Start/Structure/content.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/Base/Start/Structure/header.ts">
