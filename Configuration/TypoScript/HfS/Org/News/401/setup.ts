0f3f0073c103c9cd6ccf1a35f6dd0dbf6c3ea1b8
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-12 20:43:06 +0000

    6.0.2 +++ CSS
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87269 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Configuration/TypoScript/HfS/Org/News/401/setup.ts
@@ -0,0 +1,29 @@
+plugin.tx_browser_pi1.views.list.401.tx_org_news.title.10.30
+  views {
+    list {
+      401 {
+        tx_org_news {
+          title {
+            10 {
+              31 = COA
+              31 {
+                10 = TEXT
+                10 {
+                  value   = News from
+                  lang.de = Nachricht vom
+                }
+                20 = TEXT
+                20 {
+                  value       = ###TX_ORG_NEWS.DATETIME###
+                  strftime    = %d. %B
+                  noTrimWrap  = | ||
+                }
+                wrap = <div class="hfsnewsdate">|</div>
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
