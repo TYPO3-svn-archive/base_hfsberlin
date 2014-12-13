6e159cfaf1811c32cfe89dbee17ba6d060d8aa54
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-13 12:30:44 +0000

    6.0.2 +++ SASS, Foundation 5.5.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87323 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/HfS/Structure/content.ts
+++ Configuration/TypoScript/HfS/Structure/content.ts
@@ -22,60 +22,65 @@ base_hfsberlin {
       }
       main = COA
       main {
+          // header (3)
         10 = COA
         10 {
-          10 < base_hfsberlin.menu.menuIconsTop
-          20 = HMENU
+            // quicknav
+          10 = COA
+          10 {
+            10 < base_hfsberlin.menu.menuIconsTop
+            20 = HMENU
+            20 {
+              special = directory
+              special {
+                value = {$base_hfsberlin.pages.libraries.quicknav}
+              }
+              maxItems  = 1
+              begin     = 1
+              1 < base_hfsberlin.menu.tmenu
+              1.IFSUB     >
+              1.CURIFSUB  >
+              1.ACTIFSUB  >
+              1.NO.wrapItemAndSub (
+                          <li class="no firstitem">
+                            |
+                          </li>
+              )
+              1.CUR.wrapItemAndSub (
+                          <li class="cur firstitem">
+                            |
+                          </li>
+              )
+            }
+            21 = HMENU
+            21 {
+              special = directory
+              special {
+                value = {$base_hfsberlin.pages.libraries.quicknav}
+              }
+              begin     = 2
+              1 < base_hfsberlin.menu.tmenu
+              1.IFSUB     >
+              1.CURIFSUB  >
+              1.ACTIFSUB  >
+            }
+            wrap = <div id="quicknav" class="row show-for-medium-up">|</div>
+          }
+            // header (3): image
+          20 < styles.content.get
           20 {
-            special = directory
-            special {
-              value = {$base_hfsberlin.pages.libraries.quicknav}
+            select {
+              where = colPos=3
             }
-            maxItems  = 1
-            begin     = 1
-            1 < base_hfsberlin.menu.tmenu
-            1.IFSUB     >
-            1.CURIFSUB  >
-            1.ACTIFSUB  >
-            1.NO.wrapItemAndSub (
-                        <li class="no firstitem">
-                          |
-                        </li>
-            )
-            1.CUR.wrapItemAndSub (
-                        <li class="cur firstitem">
-                          |
-                        </li>
-            )
-          }
-          21 = HMENU
-          21 {
-            special = directory
-            special {
-              value = {$base_hfsberlin.pages.libraries.quicknav}
+            stdWrap {
+              required  = 1
+              wrap      = <div class="row show-for-medium-up"><div id="headerimage">|</div></div>
             }
-            begin     = 2
-            1 < base_hfsberlin.menu.tmenu
-            1.IFSUB     >
-            1.CURIFSUB  >
-            1.ACTIFSUB  >
           }
-          wrap = <div id="quicknav" class="row">|</div>
         }
-        // table, select, stdWrap
+        // content (0)
         20 < styles.content.get
         20 {
-          select {
-            where = colPos=3
-          }
-          stdWrap {
-            required  = 1
-            wrap      = <div class="row"><div id="headerimage">|</div></div>
-          }
-        }
-        // table, select, stdWrap
-        30 < styles.content.get
-        30 {
           select {
             where = colPos=0
           }
