d76dc171182e71f56bdaa95a01953257a5283a55
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-14 12:05:51 +0000

    6.0.2 +++ 401
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87396 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/HfS/Org/News/401/single/setup.ts
+++ Configuration/TypoScript/HfS/Org/News/401/single/setup.ts
@@ -1,1 +1,2 @@
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/HfS/Org/News/401/single/htmlSnippets.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/HfS/Org/News/401/single/tableFields/tx_org_news.ts">
