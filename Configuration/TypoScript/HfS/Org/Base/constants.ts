29ecb4966c5593de8f73c63d209d86d45e970277
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-14 21:36:54 +0000

    6.0.2 +++ template Org Base
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87427 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Configuration/TypoScript/HfS/Org/Base/constants.ts
@@ -0,0 +1,14 @@
+plugin.org {
+  pages {
+    headquarter = 20
+    news        =  1
+    staff       = 22
+  }
+  sysfolder {
+    calendar    = 116
+    headquarter = 119
+    location    = 120
+    news        = 117
+    staff       = 114
+  }
+}
