7e217a429afc6d5ff589820c4de403508115b6f6
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-14 12:08:23 +0000

    6.0.2 +++ 401
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87397 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Configuration/TypoScript/HfS/Org/News/401/single/htmlSnippets.ts
@@ -0,0 +1,33 @@
+plugin.tx_browser_pi1 {
+  views {
+    single {
+      401 {
+        htmlSnippets {
+          subparts {
+            singleview >
+            singleview = TEXT
+            singleview {
+              value (
+                <!-- ###SINGLEVIEW### begin --><!-- ###SINGLEBODY### begin --><!-- ###SINGLEBODYROW### begin -->
+                <!-- ###AREA_FOR_AJAX_LIST_01### begin -->
+                <div class="main columns small-12">
+                  ###RECORD_BROWSER###
+                  ###TX_ORG_NEWS.TITLE###
+                  <!-- ###AREA_FOR_AJAX_LIST_01### end -->
+                  <!-- ###BACKBUTTON### begin -->
+                  <p class="backbutton">
+                    ###MY_SINGLEVIEWBACKBUTTON###
+                  </p>
+                  <!-- ###BACKBUTTON### end -->
+                  <!-- ###AREA_FOR_AJAX_LIST_02### begin -->
+                </div>
+                <!-- ###AREA_FOR_AJAX_LIST_02### end -->
+                <!-- ###SINGLEBODYROW### end --><!-- ###SINGLEBODY### end --><!-- ###SINGLEVIEW### end -->
+)
+            }
+          }
+        }
+      }
+    }
+  }
+}
