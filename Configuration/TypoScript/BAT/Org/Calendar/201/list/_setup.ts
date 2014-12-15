7ef1eea01fc805c1d868c145d3db8ba3e8804dbc
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-15 22:40:22 +0000

    6.0.7 +++ Upgrade Org 6.0.7
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87525 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/BAT/Org/Calendar/201/list/_setup.ts
+++ Configuration/TypoScript/BAT/Org/Calendar/201/list/_setup.ts
@@ -1,4 +1,4 @@
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/BAT/Org/Cal/201/list/sql.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/BAT/Org/Cal/201/list/tableFields.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/BAT/Org/Cal/201/list/filter.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/BAT/Org/Cal/201/list/htmlSnippets.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/BAT/Org/Calendar/201/list/sql.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/BAT/Org/Calendar/201/list/tableFields.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/BAT/Org/Calendar/201/list/filter.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/BAT/Org/Calendar/201/list/htmlSnippets.ts">
