387dd1c32e1578386fe5889c07b7cb934db01e19
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-15 10:10:19 +0000

    6.0.2 +++ 511
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87474 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/BAT/Page/Foundation/setup.ts
+++ Configuration/TypoScript/BAT/Page/Foundation/setup.ts
@@ -1,6 +1,6 @@
 page {
   includeCSS {
-    base_hfsberlin5x = EXT:base_hfsberlin/Resources/Public/Css/hfs-foundation.css
+    base_hfsberlin_bat_foundation = {$base_hfsberlin.bat.path.css.foundation}
   }
   includeJSlibs {
     base_hfsberlin5xModernizr = EXT:base_hfsberlin/Resources/Public/JavaScript/Libs/Foundation-5.5.0/js/modernizr.js
