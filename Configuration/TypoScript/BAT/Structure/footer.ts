2985872957e8da39bcea986f664c6adff7394e75
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-16 01:59:41 +0000

    6.0.7 +++ Upgrade Org 6.0.7
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87552 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/BAT/Structure/footer.ts
+++ Configuration/TypoScript/BAT/Structure/footer.ts
@@ -2,32 +2,16 @@ base_hfsberlin =
 base_hfsberlin {
   bat {
     structure {
-      header = COA
-      header {
-          // Logos
-        10 = COA
+      footer = COA
+      footer {
+        10 < styles.content.get
         10 {
-          10 < styles.content.get
-          10 {
-            select {
-              pidInList = {$base_hfsberlin.bat.pages.libraries.header.logos}
-              max       = 1
-            }
+          select {
+            pidInList = {$base_hfsberlin.bat.pages.libraries.footer}
+            max       = 1
           }
-          wrap = <div id="box_header_logos" class="columns small-10 medium-7">|</div>
         }
-          // News
-        20 = COA
-        20 {
-          10 < styles.content.get
-          10 {
-            select {
-              pidInList = {$base_hfsberlin.bat.pages.libraries.header.news}
-              max       = 1
-            }
-          }
-          wrap = <div id="box_header_news" class="columns show-for-medium-up small-10 medium-3">|</div>
-        }
+        wrap = <div class="columns">|</div>
       }
     }
   }
--- Configuration/TypoScript/BAT/Structure/header.ts
+++ Configuration/TypoScript/BAT/Structure/header.ts
@@ -2,16 +2,32 @@ base_hfsberlin =
 base_hfsberlin {
   bat {
     structure {
-      footer = COA
-      footer {
-        10 < styles.content.get
+      header = COA
+      header {
+          // Logos
+        10 = COA
         10 {
-          select {
-            pidInList = {$base_hfsberlin.bat.pages.libraries.footer}
-            max       = 1
+          10 < styles.content.get
+          10 {
+            select {
+              pidInList = {$base_hfsberlin.bat.pages.libraries.header.logos}
+              max       = 1
+            }
           }
+          wrap = <div id="box_header_logos" class="columns small-10 medium-7">|</div>
         }
-        wrap = <div class="columns">|</div>
+          // News
+        20 = COA
+        20 {
+          10 < styles.content.get
+          10 {
+            select {
+              pidInList = {$base_hfsberlin.bat.pages.libraries.header.news}
+              max       = 1
+            }
+          }
+          wrap = <div id="box_header_news" class="columns show-for-medium-up small-10 medium-3">|</div>
+        }
       }
     }
   }
