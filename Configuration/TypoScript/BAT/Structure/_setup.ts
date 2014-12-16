d12081d42327aa2e6ab68f82f05e49b215405a8b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-16 01:54:53 +0000

    6.0.7 +++ Upgrade Org 6.0.7
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87550 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/BAT/Structure/_setup.ts
+++ Configuration/TypoScript/BAT/Structure/_setup.ts
@@ -1,3 +1,4 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/BAT/Structure/menu.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/BAT/Structure/content.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/BAT/Structure/header.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/BAT/Structure/footer.ts">
