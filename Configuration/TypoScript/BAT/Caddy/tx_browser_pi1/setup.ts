699dc8104e46edb13a943148d2800650f976deff
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-20 11:19:53 +0000

    6.0.7 +++ Labeling minicaddy
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87663 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Configuration/TypoScript/BAT/Caddy/tx_browser_pi1/setup.ts
@@ -0,0 +1,13 @@
+plugin.tx_caddy_pi1 {
+  _HTMLMARKER {
+    caddy_bankaccount {
+      4 {
+        value = Please pay the tickets on pick up.
+        lang {
+          de = Bitte bezahlen Sie die Karten bei der Abholung.
+          en = Please pay the tickets on pick up.
+        }
+      }
+    }
+  }
+}
---
+++ Configuration/TypoScript/BAT/Caddy/tx_browser_pi3/setup.ts
@@ -0,0 +1,24 @@
+plugin.tx_caddy_pi3 {
+  content {
+    sum {
+      20 {
+        10 {
+          data >
+          value = ticket
+          lang {
+            de = Karte
+            en = ticket
+          }
+        }
+        20 {
+          data >
+          value = tickets
+          lang {
+            de = Karten
+            en = tickets
+          }
+        }
+      }
+    }
+  }
+}
