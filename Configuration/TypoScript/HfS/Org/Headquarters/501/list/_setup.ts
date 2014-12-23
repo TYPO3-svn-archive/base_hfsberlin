37a3d6a1c0f227f1602222aadbf7f3e3e4d71ffe
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-23 20:10:40 +0000

    6.0.7 +++ 501
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87732 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/HfS/Org/Headquarters/501/list/_setup.ts
+++ Configuration/TypoScript/HfS/Org/Headquarters/501/list/_setup.ts
@@ -1,4 +1,4 @@
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:org/Configuration/TypoScript/headquarters/501/tx_browser_pi1/list/marker.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:org/Configuration/TypoScript/headquarters/501/tx_browser_pi1/list/tableFields.tsXX">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:org/Configuration/TypoScript/headquarters/501/tx_browser_pi1/list/filter.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:org/Configuration/TypoScript/headquarters/501/tx_browser_pi1/list/htmlSnippets.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/HfS/Org/Headquarters/501/list/marker.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/HfS/Org/Headquarters/501/list/tableFields.tsXX">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/HfS/Org/Headquarters/501/list/filter.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/HfS/Org/Headquarters/501/list/htmlSnippets.ts">
