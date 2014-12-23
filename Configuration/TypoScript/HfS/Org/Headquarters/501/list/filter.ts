474e8b17ca55d237424d1c8298ab611799d3aaf9
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-23 20:37:41 +0000

    6.0.7 +++ 501
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87737 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/HfS/Org/Headquarters/501/list/filter.ts
+++ Configuration/TypoScript/HfS/Org/Headquarters/501/list/filter.ts
@@ -5,6 +5,11 @@ plugin.tx_browser_pi1 {
         filter {
           radialsearch >
           tx_org_headquarterscat >
+          tx_org_headquarters {
+            title {
+              wrap = <div class="columns category_menu category_menu_treeview">|</div>
+            }
+          }
         }
       }
     }
