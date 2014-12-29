24d67d1bdbe6815b894a39fe1db7bf4b77e20871
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-29 18:05:48 +0000

    6.0.3 +++ 201
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87890 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Configuration/TypoScript/HfS/Org/Calendar/201/list/setup.ts
@@ -0,0 +1,129 @@
+plugin.tx_browser_pi1 {
+  views {
+    list {
+      201 {
+        filter {
+          radialsearch >
+        }
+        tx_org_cal {
+          title {
+            10 {
+              wrap = <div class="columns medium-9 large-9">|</div>
+            }
+            20 {
+              10 {
+                10 {
+                  10 {
+                    default {
+                      strftime = %A
+                    }
+                    notype {
+                      strftime = %A
+                    }
+                    page {
+                      strftime = %A
+                    }
+                    tx_org_event {
+                      strftime = %A
+                    }
+                    url {
+                      strftime = %A
+                    }
+                  }
+                  20 {
+                    default {
+                      strftime = %d.%m
+                    }
+                    notype {
+                      strftime = %d.%m
+                    }
+                    page {
+                      strftime = %d.%m
+                    }
+                    tx_org_event {
+                      strftime = %d.%m
+                    }
+                    url {
+                      strftime = %d.%m
+                    }
+                  }
+                  30 {
+                    default {
+                      strftime = %H:%M Uhr
+                    }
+                    notype {
+                      strftime = %H:%M Uhr
+                    }
+                    page {
+                      strftime = %H:%M Uhr
+                    }
+                    tx_org_event {
+                      strftime = %H:%M Uhr
+                    }
+                    url {
+                      strftime = %H:%M Uhr
+                    }
+                  }
+                }
+                20 {
+                  10 {
+                    default {
+                      strftime = %A
+                    }
+                    notype {
+                      strftime = %A
+                    }
+                    page {
+                      strftime = %A
+                    }
+                    tx_org_event {
+                      strftime = %A
+                    }
+                    url {
+                      strftime = %A
+                    }
+                  }
+                  20 {
+                    default {
+                      strftime = %d.%m
+                    }
+                    notype {
+                      strftime = %d.%m
+                    }
+                    page {
+                      strftime = %d.%m
+                    }
+                    tx_org_event {
+                      strftime = %d.%m
+                    }
+                    url {
+                      strftime = %d.%m
+                    }
+                  }
+                  30 {
+                    default {
+                      strftime = %H:%M Uhr
+                    }
+                    notype {
+                      strftime = %H:%M Uhr
+                    }
+                    page {
+                      strftime = %H:%M Uhr
+                    }
+                    tx_org_event {
+                      strftime = %H:%M Uhr
+                    }
+                    url {
+                      strftime = %H:%M Uhr
+                    }
+                  }
+                }
+              }
+              wrap = <div class="medium-3 large-3">|</div>
+            }
+          }
+        }
+      }
+    }
+  }
+}
