788bd6c1012e357e714b6d9dd36ed0d6c8525946
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-29 18:27:17 +0000

    6.0.7 +++ 201
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87895 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Configuration/TypoScript/HfS/Org/Calendar/201/single/setup.ts
@@ -0,0 +1,36 @@
+plugin.tx_browser_pi1 {
+  views {
+    single {
+      201 {
+        tx_org_cal {
+          datetime {
+            10 {
+              10 {
+                10 {
+                  strftime = %A
+                }
+                20 {
+                  strftime = %d.%m
+                }
+                30 {
+                  strftime = %H:%M Uhr
+                }
+              }
+              20 {
+                10 {
+                  strftime = %A
+                }
+                20 {
+                  strftime = %d.%m
+                }
+                30 {
+                  strftime = %H:%M Uhr
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
