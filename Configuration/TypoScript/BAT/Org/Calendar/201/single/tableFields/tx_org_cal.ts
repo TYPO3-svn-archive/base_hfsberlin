80536d3e6a786722793d9e492bc761a015e3484a
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-30 23:54:04 +0000

    6.0.3 +++ 201
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@88003 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/BAT/Org/Calendar/201/single/tableFields/tx_org_cal.ts
+++ Configuration/TypoScript/BAT/Org/Calendar/201/single/tableFields/tx_org_cal.ts
@@ -25,6 +25,15 @@ plugin.tx_browser_pi1 {
                     }
                     wrap = <div class="cal_datetime">|</div>
                   }
+                  30 = COA
+                  30 {
+                    field >
+                    required >
+                    10 = TEXT
+                    10 {
+                      field = tx_org_repertoire.bodytext // tx_org_event.bodytext // tx_org_cal.bodytext
+                    }
+                  }
                 }
               }
             }
