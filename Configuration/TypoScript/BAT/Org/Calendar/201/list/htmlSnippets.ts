0970cf17db51aec16f8e3111912095b96d96ffec
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-30 23:20:54 +0000

    6.0.3 +++ 201
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@87997 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/BAT/Org/Calendar/201/list/htmlSnippets.ts
+++ Configuration/TypoScript/BAT/Org/Calendar/201/list/htmlSnippets.ts
@@ -17,6 +17,23 @@ plugin.tx_browser_pi1 {
 )
             }
           }
+          subparts {
+            listview = TEXT
+            listview {
+              value (
+                <div class="row">
+                  <div id="c###TT_CONTENT.UID###-listview-###MODE###" class="columns listview listview-content listview-###MODE### listview-content-###MODE###">
+                    <!-- ###LISTBODY### begin --><!-- ###LISTBODYITEM### begin -->
+                    <div class="row">
+                      <div class="record">
+                        ###TX_ORG_CAL.TITLE###
+                      </div>
+                    </div><!-- /row --><!-- ###LISTBODYITEM### end --><!-- ###LISTBODY### end -->
+                  </div><!-- /columns --><!-- /listview -->
+                </div><!-- /row -->
+)
+            }
+          }
         }
       }
     }
