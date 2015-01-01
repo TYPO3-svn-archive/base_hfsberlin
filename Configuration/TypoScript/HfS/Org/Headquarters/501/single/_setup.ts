61017094cdd9a7e23a6ce20ddc9b7003db8c908b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-01-01 23:52:38 +0000

    6.0.3 +++ org constants
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@88021 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/HfS/Org/Headquarters/501/single/_setup.ts
+++ Configuration/TypoScript/HfS/Org/Headquarters/501/single/_setup.ts
@@ -1,2 +1,1 @@
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/HfS/Org/Headquarters/501/single/tableFields/_setup.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/HfS/Org/Headquarters/501/single/htmlSnippets.ts">
