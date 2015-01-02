7d17f325db3bd91e3ee9bde57df5e7c1efc2b227
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-01-02 01:32:49 +0000

    6.0.3 +++ 501
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@88030 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/BAT/Org/Headquarters/501/single/_setup.ts
+++ Configuration/TypoScript/BAT/Org/Headquarters/501/single/_setup.ts
@@ -1,2 +1,3 @@
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/BAT/Org/Headquarters/501/single/marker.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/BAT/Org/Headquarters/501/single/tableFields.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:base_hfsberlin/Configuration/TypoScript/BAT/Org/Headquarters/501/single/htmlSnippets.ts">
---
+++ Configuration/TypoScript/BAT/Org/Headquarters/501/single/marker.ts
@@ -0,0 +1,13 @@
+plugin.tx_browser_pi1 {
+  marker {
+    my_singleviewbackbutton {
+      30 {
+        value = All units
+        lang {
+          de = Standort
+          en = All units
+        }
+      }
+    }
+  }
+}
