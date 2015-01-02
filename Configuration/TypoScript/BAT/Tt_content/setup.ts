791218e22bd432c29da1ca6377e466b306d87bf2
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-01-02 23:18:27 +0000

    6.0.7 +++ tt_content.image workaround for src
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@88051 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/BAT/Tt_content/setup.ts
+++ Configuration/TypoScript/BAT/Tt_content/setup.ts
@@ -1,3 +1,6 @@
+  // 150102, dwildt: Without this property the src-tag doesn't contain the link to the processed image but to the original image
+tt_content.image.20.1.file.params =
+
 tt_content.table.20.stdWrap {
   addParams {
     border = 0
