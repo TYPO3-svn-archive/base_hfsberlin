4b1a1baad4c654098c92e48adca29ce1103cfea5
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-01-23 12:25:08 +0000

    6.0.10 +++ single view calendar
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/base_hfsberlin/trunk@88268 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/BAT/Org/Calendar/201/single/tableFields/tx_org_cal.ts
+++ Configuration/TypoScript/BAT/Org/Calendar/201/single/tableFields/tx_org_cal.ts
@@ -62,6 +62,33 @@ plugin.tx_browser_pi1 {
                   }
                 }
               }
+              1 {
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
+              2 {
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
+              8 {
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
+              9 {
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
+              10 {
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
+              17 {
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
+              18 {
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
+              25 {
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
+              26 {
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
             }
           }
         }
